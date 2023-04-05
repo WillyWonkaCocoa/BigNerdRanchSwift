f5f9de0198fc32527fc371a4d24b29547a3c5120
Will Gao <wgao1@umbc.edu>
2023-04-05 13:29:54 -0700

    Higher-order functions: map, filter, reduce

--- Closures.playground/Contents.swift
+++ Closures.playground/Contents.swift
@@ -27,11 +27,31 @@ func format(numbers: [Double], using formatter: (Double) -> String = {"\($0)"}) 
     return result
 }
 
+
+var numberOfTransformations = 0
+
 let rounder: (Double) -> String = {
     (num: Double) -> String in
+    numberOfTransformations += 1
     return "\(Int(num.rounded()))"
 }
 
 let volunteerAverages = [10.75, 4.2, 1.5, 12.12, 16.815]
 let roundedAveragesAsStrings = format(numbers: volunteerAverages, using: rounder)
 let exactAveragesAsStrings = format(numbers: volunteerAverages)
+print(numberOfTransformations)
+
+let roundedAverages = volunteerAverages.map {
+    (avg: Double) -> Int in
+    return Int(avg.rounded())
+}
+
+let passingAverages = roundedAverages.filter {
+    (avg: Int) -> Bool in
+    return avg >= 10
+}
+
+let estimatedParticipation = passingAverages.reduce(5) {
+    (estimationSoFar: Int, currentOrgAverage: Int) -> Int in
+    return estimationSoFar + currentOrgAverage
+}
