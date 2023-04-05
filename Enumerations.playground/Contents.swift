5b4ddf84ee93b05ecd89889e6c74b2891ec10a04
Will Gao <wgao1@umbc.edu>
2023-04-05 16:10:41 -0700

    Enum types with associated values

--- Enumerations.playground/Contents.swift
+++ Enumerations.playground/Contents.swift
@@ -96,3 +96,33 @@ print("the bulb's temperature is \(bulbTemperature)")
 bulb.toggle()
 bulbTemperature = bulb.surfaceTemperature(forAmbientTemperature: ambientTemperature)
 print("the bulb's temperature is \(bulbTemperature)")
+
+enum ShapeDimensions {
+    // point has no associated avlue - it is dimensionless
+    case point
+    
+    // square's assoicated value is the length of one side
+    case square(side: Double)
+    
+    // rectangle's associated value defines its width and height
+    case rectangle(width: Double, height: Double)
+    
+    func area() -> Double {
+        switch self {
+        case .point:
+            return 0
+        case let .square(side: side):
+            return side * side
+        case let .rectangle(width: w, height: h):
+            return w * h
+        }
+    }
+}
+
+var squareShape = ShapeDimensions.square(side: 10.0)
+var rectShape = ShapeDimensions.rectangle(width: 5.0, height: 10.0)
+var pointShape = ShapeDimensions.point
+
+print("square's area = \(squareShape.area())")
+print("rectangle's area = \(rectShape.area())")
+print("point's area = \(pointShape.area())")
